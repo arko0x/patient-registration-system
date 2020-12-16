3311ff506b49e354017b3f4aaf3ef0b010d2c4fc
arko0x <arkadiusz88084@gmail.com>
2020-12-16 13:47:40 +0100

    Initial commit

---
+++ src/main/java/pl/nikodem/patientregistrationsystem/PatientRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.nikodem.patientregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRegistrationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientRegistrationSystemApplication.class, args);
+    }
+
+}
