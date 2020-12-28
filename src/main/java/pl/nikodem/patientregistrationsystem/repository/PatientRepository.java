c66329c52f87c1444f07c431456921b424a0807d
arko0x <arkadiusz88084@gmail.com>
2020-12-28 18:09:46 +0100

    add Doctor and Patient repositories

---
+++ src/main/java/pl/nikodem/patientregistrationsystem/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package pl.nikodem.patientregistrationsystem.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.nikodem.patientregistrationsystem.entity.Patient;
+
+@Repository
+public interface PatientRepository extends CrudRepository<Patient, Long> {
+    Patient findByUsername(String username);
+}
