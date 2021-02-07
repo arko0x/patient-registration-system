b59d303d2b0db0e2547b057daee23de2619687b7
arko0x <arkadiusz88084@gmail.com>
2021-02-07 17:24:35 +0100

    add check if account is enabled and not locked

--- src/main/java/pl/nikodem/patientregistrationsystem/security/DoctorAuthenticationProvider.java
+++ src/main/java/pl/nikodem/patientregistrationsystem/security/DoctorAuthenticationProvider.java
@@ -1,24 +1,22 @@
 package pl.nikodem.patientregistrationsystem.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import pl.nikodem.patientregistrationsystem.service.DoctorDetailsService;
+import pl.nikodem.patientregistrationsystem.doctor.DoctorService;
 
 @Component
 public class DoctorAuthenticationProvider implements AuthenticationProvider {
-    private final DoctorDetailsService doctorService;
+    private final DoctorService doctorService;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public DoctorAuthenticationProvider(DoctorDetailsService doctorService, PasswordEncoder passwordEncoder) {
+    public DoctorAuthenticationProvider(DoctorService doctorService, PasswordEncoder passwordEncoder) {
         this.doctorService = doctorService;
         this.passwordEncoder = passwordEncoder;
     }
@@ -32,7 +30,13 @@ public class DoctorAuthenticationProvider implements AuthenticationProvider {
 
         if (doctor != null) {
             if (passwordEncoder.matches(password, doctor.getPassword()))
-                return new UsernamePasswordAuthenticationToken(username, password, ApplicationUserRole.DOCTOR.getAuthorities());
+                if (doctor.isEnabled() && doctor.isAccountNonLocked()) {
+                    return new UsernamePasswordAuthenticationToken(username, password, ApplicationUserRole.DOCTOR.getAuthorities());
+                }
+                else if (!doctor.isEnabled())
+                    throw new DisabledException("Account disabled");
+                else
+                    throw new LockedException("Account is locked");
             else throw new BadCredentialsException("Incorrect password for user " + doctor.getUsername());
         }
         else throw new UsernameNotFoundException("Username " + username + " not found");
