0c0522e38c2717473229340ef4ca1be2116c0f69
arko0x <arkadiusz88084@gmail.com>
2021-02-17 01:15:33 +0100

    add deleteByStartTimeAndDoctor method

--- src/main/java/pl/nikodem/patientregistrationsystem/appointments/MeetingIntervalRepository.java
+++ src/main/java/pl/nikodem/patientregistrationsystem/appointments/MeetingIntervalRepository.java
@@ -3,10 +3,13 @@ package pl.nikodem.patientregistrationsystem.appointments;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import pl.nikodem.patientregistrationsystem.doctor.Doctor;
 import pl.nikodem.patientregistrationsystem.doctor.MeetingInterval;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public interface MeetingIntervalRepository extends CrudRepository<MeetingInterval, Long> {
     Page<MeetingInterval> findByDate(LocalDate date, Pageable pageable);
+    void deleteByStartTimeAndDoctor(LocalTime startTime, Doctor doctor);
 }
