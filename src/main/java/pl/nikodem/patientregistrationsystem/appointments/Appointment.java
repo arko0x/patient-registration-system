35cd2b8ff70f690580748d358df32a896ae20025
arko0x <arkadiusz88084@gmail.com>
2021-02-17 01:14:06 +0100

    change appointment date field type from Instant to LocalDateTime

--- src/main/java/pl/nikodem/patientregistrationsystem/appointments/Appointment.java
+++ src/main/java/pl/nikodem/patientregistrationsystem/appointments/Appointment.java
@@ -1,6 +1,5 @@
 package pl.nikodem.patientregistrationsystem.appointments;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,7 +8,7 @@ import pl.nikodem.patientregistrationsystem.doctor.Doctor;
 import pl.nikodem.patientregistrationsystem.patient.Patient;
 
 import javax.persistence.*;
-import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -28,9 +27,9 @@ public class Appointment {
     @JsonManagedReference
     private Patient patient;
 
-    private Instant date;
+    private LocalDateTime date;
 
-    public Appointment(Doctor doctor, Patient patient, Instant date) {
+    public Appointment(Doctor doctor, Patient patient, LocalDateTime date) {
         this.doctor = doctor;
         this.patient = patient;
         this.date = date;
