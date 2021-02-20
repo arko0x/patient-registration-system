f3f07da4378ac74e6b8d5291fc0f31ba1cca2074
arko0x <arkadiusz88084@gmail.com>
2021-02-20 23:58:43 +0100

    Delete unnecessary files

--- src/main/java/pl/nikodem/patientregistrationsystem/patient/registration/email/ConfirmationEmailGenerator.java
+++
@@ -1,8 +0,0 @@
-package pl.nikodem.patientregistrationsystem.patient.registration.email;
-
-public class ConfirmationEmailGenerator {
-    public static String generateEmailMessage(String username, String token) {
-        return "<div>Confirm your email, " + username + "!</div><br>" +
-                "<div><a href = \"localhost:8080/patient/register/confirm?token=" + token + "\">Confirmation link </a></div>";
-    }
-}
